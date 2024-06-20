eab1c1daea041d7e05f4780b11604dbba1f5f75d
NicatDev <nicat254memmedov@gmail.com>
2024-06-20 23:36:48 +0400

    one step more

--- project/settings.py
+++ project/settings.py
@@ -118,9 +118,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'az'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Baku'
+SITE_ID = 1
 
 USE_I18N = True
 
