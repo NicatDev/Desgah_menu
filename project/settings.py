f29336f9d88cb4a5a0d3b845599620adcb49e3f0
NicatDev <nicat254memmedov@gmail.com>
2024-06-20 23:33:30 +0400

    one step more

--- project/settings.py
+++ project/settings.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'menu'
 ]
 
@@ -131,10 +132,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'mediafiles')
